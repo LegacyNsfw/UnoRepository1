205bba9a374acac9ca283e92924177c7a967f8b4
nate <nate@whatever.net>
2024-10-05 01:22:05 -0700

    Initial commit. Settings page is mocked up.

---
+++ UnoExperiment1/Presentation/MainModel.cs
@@ -0,0 +1,32 @@
+namespace UnoExperiment1.Presentation;
+
+public partial record MainModel
+{
+    private INavigator _navigator;
+
+    public MainModel(
+        IStringLocalizer localizer,
+        IOptions<AppConfig> appInfo,
+        INavigator navigator)
+    {
+        _navigator = navigator;
+        Title = localizer["ApplicationName"] + " - Release 25";
+    }
+
+    public string? Title { get; }
+
+    public IState<string> Name => State<string>.Value(this, () => string.Empty);
+
+    public async Task GoToSecond()
+    {
+        var name = await Name;
+        await _navigator.NavigateViewModelAsync<SecondModel>(this, data: new Entity(name!));
+    }
+
+    public async Task GoToSettings()
+    {
+        var name = await Name;
+        await _navigator.NavigateViewModelAsync<SettingsModel>(this);
+    }
+
+}
